85cdcd031581a2ae664e8e85066d01cd377a1d61
Guyutongxue <guyutongxue@163.com>
2025-01-23 00:40:10 +0800

    hotfix

--- packages/webui-core/unocss.config.ts
+++ packages/webui-core/unocss.config.ts
@@ -16,22 +16,20 @@
 // import { defineConfig, presetUno } from "unocss";
 // import { parseColor, colorToString } from "@unocss/preset-mini/utils";
 
-// export default defineConfig({
-//   presets: [presetUno()],
-//   rules: [
-//     [
-//       /^btn-(.*)$/,
-//       ([, c], { theme }) => {
-//         const data = parseColor(c, theme, "colors");
+export default defineConfig({
+  presets: [presetUno()],
+  rules: [
+    [
+      /^btn-(.*)$/,
+      ([, c], { theme }) => {
+        const data = parseColor(c, theme, "colors");
 
-//         if (data?.cssColor) {
-//           return {
-//             "--btn-color": colorToString(data.cssColor),
-//           };
-//         }
-//       },
-//     ],
-//   ],
-// });
-import "css-tree/definition-syntax-data-patch";
-export default {}
+        if (data?.cssColor) {
+          return {
+            "--btn-color": colorToString(data.cssColor),
+          };
+        }
+      },
+    ],
+  ],
+});
