c1260528d68aa355b44b38c2d64ea8b4b3d44242
Guyutongxue <guyutongxue@163.com>
2025-01-23 00:42:53 +0800

    hotfix 2

--- packages/webui-core/unocss.config.ts
+++ packages/webui-core/unocss.config.ts
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// import { defineConfig, presetUno } from "unocss";
-// import { parseColor, colorToString } from "@unocss/preset-mini/utils";
+import { defineConfig, presetUno } from "unocss";
+import { parseColor, colorToString } from "@unocss/preset-mini/utils";
 
 export default defineConfig({
   presets: [presetUno()],
